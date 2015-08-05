fbd4d0af59ee98c4427397df323f56f05f443755
mreeves <michael.reeves@mandiant.com>
2015-08-05 16:08:55 -0400

    Created CS install script

--- opt/onionsalt/salt/sensor/scripts/cstackinstall.sh
+++ opt/onionsalt/salt/sensor/scripts/cstackinstall.sh
@@ -5,6 +5,7 @@
 # Check for the drop....
 if [ ! -f /etc/state/cstack.txt ]; then
 curl https://packagecloud.io/install/repositories/criticalstack/critical-stack-intel/script.deb.sh | bash
-sudo apt-get install critical-stack-intel
+apt-get install critical-stack-intel
+critical-stack-intel api {{ salt['pillar.get']('sensorstuff:cskey', '') }}
 touch /etc/state/cstack.txt
 fi
