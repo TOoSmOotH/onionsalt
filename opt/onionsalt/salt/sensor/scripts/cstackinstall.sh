0e926bad7f8becf468e40f6794e7a222b9d06b6f
mreeves <michael.reeves@mandiant.com>
2015-08-05 15:22:40 -0400

    Client Install Script
    I need to test this.

--- opt/onionsalt/salt/sensor/scripts/cstackinstall.sh
+++ opt/onionsalt/salt/sensor/scripts/cstackinstall.sh
@@ -1,3 +1,10 @@
 ###
 ## Script for Critical Stack Intel Client install
 ##
+
+# Check for the drop....
+if [ ! -f /etc/state/cstack.txt ]; then
+curl https://packagecloud.io/install/repositories/criticalstack/critical-stack-intel/script.deb.sh | bash
+sudo apt-get install critical-stack-intel
+touch /etc/state/cstack.txt
+fi
